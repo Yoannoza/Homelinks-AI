40f0e08d191a567f663aa414d2843f7234522baf
Yoannoza <yoannoza25@gmail.com>
2025-01-28 18:52:45 +0100

    First Commit

---
+++ tts.py
@@ -0,0 +1,44 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+# Charger les variables d'environnement
+load_dotenv()
+
+def speech(text):
+    # Variables depuis .env
+    XI_API_KEY = os.getenv("XI_API_KEY")
+    VOICE_ID = "FvmvwvObRqIHojkEGh5N"
+    OUTPUT_PATH = "output.mp3"
+    CHUNK_SIZE = 1024
+
+    # Construire l'URL de l'API TTS
+    tts_url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}/stream"
+
+    headers = {
+        "Accept": "application/json",
+        "xi-api-key": XI_API_KEY
+    }
+
+    data = {
+        "text": text,
+        "model_id": "eleven_multilingual_v2",
+        "voice_settings": {
+            "stability": 0.5,
+            "similarity_boost": 0.8,
+            "style": 0.0,
+            "use_speaker_boost": True
+        }
+    }
+
+    try:
+        response = requests.post(tts_url, headers=headers, json=data, stream=True)
+        response.raise_for_status()
+
+        with open(OUTPUT_PATH, "wb") as f:
+            for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+                f.write(chunk)
+
+        print("Audio stream saved successfully.")
+    except requests.exceptions.RequestException as e:
+        print(f"Error in TTS request: {e}")
